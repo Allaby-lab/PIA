7668d6c7399fb8ae18a83f3fe060e438b494d539
Allaby Lab <r.cribdon@warwick.ac.uk>
2019-09-04 22:10:55 +0100

    Significant changes to the what and how
    First, the PIA no longer takes BLAST files in the default output format (0). Instead, run your blast with -outfmt "6 std staxid". This is tabular format and, crucially, identifies each hit by taxonomic ID. This saves the PIA a lot of processing.
    
    Second, if a read has multiple hits with the same E value, previously it took the top one and ignored the rest, since the sequence is equally similar to all of them. Now, it uses all of those hits to make a new "hit" from their intersection. Any new taxa in those hits do still count towards the taxonomic diversity score.
    
    Third, intersections are now calculated between any pair of taxa. Previously, taxa of rank class or lower had to be at least in the same class as each other. Now there is no limitation on what intersections are presented.

--- PIA_index_maker.pl
+++ PIA_index_maker.pl
@@ -6,7 +6,7 @@
 ####### Creates DBM index files for later use ########
 ############## Becky Cribdon, UoW 2019################
 ######################################################
-############## Version 4.6, 2019-08-22 ###############
+############## Version 4.8, 2019-09-04 ###############
 ######################################################
 
 	use strict;
@@ -21,15 +21,14 @@
 # It will take a few minutes.
 
 # Summary:
-# - Makes an index of the names file where keys are names and values are any matching taxonomic IDs (tab-delimited).
 # - Makes an index of the names file where keys are IDs and values are the matching scientific names.
 # - Makes an index of the nodes file where keys are IDs and values are parent nodes and taxonomic ranks.
 
 my %options=();
-getopts('n:N:a"', \%options);
+getopts('n:N:"', \%options);
     
 ##### Locate reference files #####
-	# If they aren't in their default location (Reference_files/), use the options to specify where they are.	
+	# If they aren't in their default location (Reference_files/), use the options to specify where they are.
 	my $nodes_filename = 'Reference_files/nodes.dmp';
 	if ($options{n}){
 		$nodes_filename = ($options{n});
@@ -38,11 +37,6 @@ getopts('n:N:a"', \%options);
     my $names_filename = 'Reference_files/names.dmp';   
 	if ($options{N}){
 		$names_filename = ($options{N});
-	}
-
-    my $abbreviations_filename = 'Reference_files/Species_abbreviations.txt';
-    if ($options{a}){
-		$abbreviations_filename = ($options{a});
 	}
 
 
@@ -83,73 +77,22 @@ while (1) { # Run this loop until "last" is called.
 close $nodes_filehandle;
 untie %nodesfileDBM;
 
-
-# Names index where keys are names
-#---------------------------------
-# This will be used to look up the taxonomic IDs of BLAST hits.
-my $namesfileDBMnames = $names_filename . '_names.dbm'; # Including $header_filename in the DBM filename ensures that every thread makes its own index file, preventing read/write conflict. Otherwise I'd have to get into file locks and oh my goodness not now.
-        
-if (-e $namesfileDBMnames) {
-    print "$namesfileDBMnames already exists. Overwriting.\n";
-    unlink $namesfileDBMnames;
-}
-        
-my %namesfileDBMnames = (); # Keys are names (scientific and not, because BLAST contains all sorts) and values are the corresponding taxonomic ID. It is built as a hash and then written to the DBM file.
-tie (%namesfileDBMnames, "DB_File", $namesfileDBMnames, O_RDWR|O_CREAT, 0666, $DB_BTREE) or die "Can't open $namesfileDBMnames.\n$!\n"; # Open the DBM file (in read-write mode; O_RDWR). If it doesn't exist, create one (O_CREAT). Note that these options use the Fcntl module.
-
-# Now populate the hash:
-open (my $names_filehandle, $names_filename) or die "Could not open $names_filename.\n$!\n";
-while (1) { # Run this loop until "last" is called.
-        my $line = <$names_filehandle>; # Read the next line from the names file.
-        if (! defined $line) { last }; # If there is no next line, exit the loop. You've processed the whole file.
-
-        my @line = split(/\|/, $line); # Split the line by | characters.
-        my $name_full = $line[1]; # Pick out the name.
-        $name_full =~ s/^\s+|\s+$//g; # Trim whitespace (tabs) off the start and end.
-        my $name; # We may have to shorten it.
-
-        if (length ($name_full) > 52) {
-            $name = substr ($name_full, 0, 52); # We only want the first 52 characters of the name (counting from 0) because that's the maximum length for most BLAST hits. Hits can be up to 55 characters intact, but 56 or higher get truncated so an ellipsis (...) can go on the end.
-            # A few taxa have very long names that only differ at the very end; so far, it seems these don't match and are discarded.
-            my @name = split (' ', $name); # Now split on spaces. This also removes any trailing space left by the character trim.
-            if ($name[1]) {
-                pop @name; # Remove the last word, which since $name_full was more than 52 characters, will have been truncated.
-            }
-            $name = join (' ', @name);
-        } else {
-            $name = $name_full;
-        }
-
-        my $ID = $line[0]; # Also get the ID.
-        $ID =~ s/^\s+|\s+$//g;
-
-        if (exists $line[2]) { # Examine the second names field. This is usually empty, but otherwise gives clarification if the name is also used for another taxon. Or other extra information that isn't relevant to us. But a second names field suggests that this $name may be a duplicate, so append this ID rather than potentially overwriting a previous one.
-            if (exists $namesfileDBMnames{$name}) {
-                $namesfileDBMnames{$name} = $namesfileDBMnames{$name} . "\t$ID";
-            } else {
-                $namesfileDBMnames{$name} = "$ID";
-            }
-        } # Only doing a hash lookup if there's a second names field saves us doing a hash lookup every time we add to the hash.
-}
-close $names_filehandle;
-untie %namesfileDBMnames;
-
     
 # Names index where keys are IDs
 #-------------------------------
 # This will be used to look up the scientific names of taxa using their IDs.
-my $namesfileDBMids = $names_filename . '_IDs.dbm';
+my $namesfileDBM = $names_filename . '.dbm';
 
-if (-e $namesfileDBMids) {
-    print "$namesfileDBMids already exists. Overwriting.\n";
-    unlink $namesfileDBMids;
+if (-e $namesfileDBM) {
+    print "$namesfileDBM already exists. Overwriting.\n";
+    unlink $namesfileDBM;
 }
    
-my %namesfileDBMids = (); # Keys are taxonomic IDs and values are scientific names.
-tie (%namesfileDBMids, "DB_File", $namesfileDBMids, O_RDWR|O_CREAT, 0666, $DB_BTREE) or die "Can't open $namesfileDBMids.\n$!\n";
+my %namesfileDBM = (); # Keys are taxonomic IDs and values are scientific names.
+tie (%namesfileDBM, "DB_File", $namesfileDBM, O_RDWR|O_CREAT, 0666, $DB_BTREE) or die "Can't open $namesfileDBM.\n$!\n";
 
 # Now populate the hash:
-open ($names_filehandle, $names_filename) or die "Could not open $names_filename.\n$!\n";
+open (my $names_filehandle, $names_filename) or die "Could not open $names_filename.\n$!\n";
 while (1) { # Run this loop until "last" is called.
         my $line = <$names_filehandle>; # Read the next line from the names file.
         if (! defined $line) { last }; # If there is no next line, exit the loop. You've processed the whole file.
@@ -161,37 +104,11 @@ while (1) { # Run this loop until "last" is called.
             $name =~ s/^\s+|\s+$//g; # Trim whitespace off the start and end.
             my $ID = $line[0];
             $ID =~ s/^\s+|\s+$//g;
-            $namesfileDBMids{$ID} = $name; # Store in the hash.
+            $namesfileDBM{$ID} = $name; # Store in the hash.
         } # If that line is not for a scientific name, ignore it and move on.
 }
 close $names_filehandle;
-untie %namesfileDBMids;
-
-
-# Species abbreviations index
-#----------------------------
-# BLAST hits will be checked against this list and assigned the corresponding ID if they match. Species abbreviations are not usually picked up by the normal names search in PIA_inner.pl.
-my $abbreviationsDBM = $abbreviations_filename . '.dbm';
-
-if (-e $abbreviationsDBM) {
-    print "$abbreviationsDBM already exists. Overwriting.\n";
-    unlink $abbreviationsDBM;
-}
-   
-my %abbreviationsDBM = (); # Keys are species abbreviations and values are taxonomic IDs.
-tie (%abbreviationsDBM, "DB_File", $abbreviationsDBM, O_RDWR|O_CREAT, 0666, $DB_BTREE) or die "Can't open $abbreviationsDBM.\n$!\n";
-
-# Now populate the hash:
-open (my $abbreviations_filehandle, $abbreviations_filename) or die "Could not open $abbreviations_filename.\n$!\n";
-while (1) { # Run this loop until "last" is called.
-        my $line = <$abbreviations_filehandle>; # Read the next line from the names file.
-        if (! defined $line) { last }; # If there is no next line, exit the loop. You've processed the whole file.
-
-        my @line = split("\t", $line); # Split the line on tabs.
-        $abbreviationsDBM{$line[0]} = $line[1]; # Save the abbreviation as the key and the ID as the value. The final field is a comment.
-}
-close $abbreviations_filehandle;
-untie %abbreviationsDBM;
+untie %namesfileDBM;
 
 
 print "Finished indexing.\n\n";  
